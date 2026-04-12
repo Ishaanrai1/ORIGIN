e332ce8e58858e1b19b41ff9ca864991db7065db
Ishaan Rai <raiishaan48@gmail.com>
2026-04-12 19:12:20 +0530

    Day 5 backend setup

---
+++ DAY-5/server.js
@@ -0,0 +1,22 @@
+/*
+server ko start karna
+sever se db connect karna
+ */
+const app = require("./src/app")
+const mongoose = require("mongoose")
+
+
+function connectToDb() {
+  mongoose.connect("mongodb://ISHAAN:ishaan2604@ac-zf6dc6i-shard-00-00.kpdpcrp.mongodb.net:27017,ac-zf6dc6i-shard-00-01.kpdpcrp.mongodb.net:27017,ac-zf6dc6i-shard-00-02.kpdpcrp.mongodb.net:27017/?ssl=true&replicaSet=atlas-4eyuii-shard-0&authSource=admin&appName=Cluster0/day6")
+    .then(() => {
+      console.log("ho gya")
+    })
+    .catch((err) => {
+      console.log("Connection failed:", err.message)  // Add this
+    })
+}
+connectToDb()
+
+app.listen(3000, () => {
+    console.log("server is running")
+})
---
+++ DAY-5/src/app.js
@@ -0,0 +1,10 @@
+/*
+server ko create karna 
+serverko config karna
+*/
+
+const express = require("express")
+const app = express()
+
+
+module.exports = app
