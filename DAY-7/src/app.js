96d9f023ebce6f018af9440778e6d264399ea503
Ishaan Rai <raiishaan48@gmail.com>
2026-04-21 18:53:10 +0530

    day 7 update

--- DAY-7/src/app.js
+++ DAY-7/src/app.js
@@ -19,6 +19,15 @@ app.post("/notes", async(req,res)=>{
 
 })
 
+app.get("/notes", async(req,res)=>{
+    const notes = await noteModel.find()
+
+    res.status(200).json({
+        message:"Notes fetched success",
+        notes
+    })
+})
+
 
 
 module.exports = app
